d82cd00e7bdaa983456e57826026aa1d581e333b
Phothonx <86020708+Phothonx@users.noreply.github.com>
2025-05-22 15:49:48 +0200

    basic structure

--- modules/bar.py
+++ modules/bar.py
@@ -1,11 +1,41 @@
 import fabric
 from fabric.widgets.wayland import WaylandWindow as Window
 
+from fabric.widgets.box import Box
+from fabric.widgets.centerbox import CenterBox
+
+from fabric.hyprland.widgets import Workspaces, WorkspaceButton
+
 class Bar(Window):
   def __init__(self, **kwargs):
     super().__init__(
+      name = "bar",
       layer="top",
       anchor="left top right",
       exclusivity="auto",
+      visible = True,
+      all_visible = True,
       **kwargs
     )
+
+    self.workspaces = Workspaces(
+      buttons = [ WorkspaceButton(id=i, label=str(i)) for i in range(1, 10) ],
+      empty_scroll=True,
+    )
+
+    self.bar_inner = CenterBox(
+      name = "bar_inner",
+      h_align="fill",
+      v_align="fill",
+      start_children = Box(
+        children = [
+          self.workspaces
+        ]
+      ),
+      center_children = None,
+      end_children = None,
+    )
+
+    self.children = [
+      self.bar_inner
+    ]
